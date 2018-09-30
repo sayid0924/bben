388234af9829ad7d02bf9aa32c2a80897b57fc3b
sayid0924 <1873588839@qq.com>
2018-09-30 17:03:17 +0800

    第一次提交

--- src/main/java/com/bben/controller/MainController.java
+++ src/main/java/com/bben/controller/MainController.java
@@ -24,6 +24,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
+/**
+ * @author 用户管理
+ */
+
 @Api(name = "用户管理", order = 1)
 @RestController
 public class MainController {
@@ -36,6 +40,7 @@ public class MainController {
     @Autowired
     private UserService personService;
 
+
     @ApiAction(name = "查询所有数据", mapping = "all", method = Method.GET)
     @ApiRespParams({
             @ApiParam(name = "code", dataType = DataType.NUMBER, defaultValue = "0", description = "状态编码"),
@@ -46,23 +51,21 @@ public class MainController {
     })
 
     @RequestMapping("/all")
-
     public JSONObject findAll() {
-
         List<User> users = userMapper1.findAll();
-        BaseResult.success(users);
         JSONObject json = new JSONObject();
         json.put("datas", BaseResult.success(users));
+        logger.info("vvvvvvvvvvvvvvvvv");
         return json;
-
     }
 
     @ApiAction(name = "查询分页数据", mapping = "page", method = Method.GET)
+    // 请求参数
     @ApiReqParams(type = ParamType.JSON,  value = {
-            @ApiParam(name = "start", dataType = DataType.STRING, defaultValue = "", description = "起始item",required = true),
-            @ApiParam(name = "end", dataType = DataType.STRING, defaultValue = "", description = "返回总item",required = true)
+            @ApiParam(name = "start", dataType = DataType.STRING,  description = "起始item",required = true),
+            @ApiParam(name = "end", dataType = DataType.STRING, description = "返回总item",required = true)
     })
-
+   // 返回参数
     @ApiRespParams({
             @ApiParam(name = "code", dataType = DataType.NUMBER, defaultValue = "0", description = "状态编码"),
             @ApiParam(name = "message", dataType = DataType.STRING, defaultValue = "操作成功", description = "提示信息"),
@@ -71,7 +74,7 @@ public class MainController {
             @ApiParam(name = "pageSize", dataType = DataType.NUMBER,description = "每页的数量",belongTo = "user"),
             @ApiParam(name = "pages", dataType = DataType.NUMBER,description = "总页数",belongTo = "user"),
             @ApiParam(name = "total", dataType = DataType.NUMBER,description = "总记录数",belongTo = "user"),
-            @ApiParam(name = "list", dataType = DataType.OBJECT,description = "",object = "data",belongTo = "user"),
+            @ApiParam(name = "list", dataType = DataType.OBJECT,object = "data",belongTo = "user"),
             @ApiParam(name = "id", dataType = DataType.NUMBER,description = "ID",belongTo = "data"),
             @ApiParam(name = "name", dataType = DataType.STRING,description = "姓名",belongTo = "data"),
             @ApiParam(name = "isLastPage", dataType = DataType.NUMBER,description = "是否为最后一页",belongTo = "user"),
